3d8f7755a1e396738990a3b2217105382b69c558
Louise Hjelstroem <Louiseh_83@hotmail.com>
2019-04-17 17:40:32 +0200

    initial commit

---
+++ CH2/02_05/start/src/components/Lost.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Lost = () => {
+    return (
+        <div>
+            <h1 style={{color: "white"}}>
+                You´re lost! Get back...get..back..now!!!
+            </h1>
+            <img style={{ paddingTop: "50px"}} src="/images/biohazard.png" alt="Back image"/>
+        </div>
+
+    )
+}
+ 
+export default Lost;
