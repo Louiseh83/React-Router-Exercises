ac04ad8833c21c1c0681b167700f2a33d6abf62d
Louise Hjelstroem <Louiseh_83@hotmail.com>
2019-04-21 18:28:32 +0200

    Ändrade lite i texten

--- CH2/02_06/start/src/components/Lost.js
+++ CH2/02_06/start/src/components/Lost.js
@@ -4,7 +4,7 @@ const Lost = () => {
     return (
         <div>
             <h1 style={{color: "white"}}>
-                You're lost! Get back...get..back..now!!!
+                You're lost! Stay away from here and go back....NOW!!!
             </h1>
             <img style={{ paddingTop: "50px"}} src="/images/biohazard.png" alt="Back image"/>
         </div>
